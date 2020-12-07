57dd318e4bc17da0069b396c375b603f313f176d
D. Khuê Lê-Huu <huudienkhue.le@gmail.com>
2020-12-07 17:12:23 +0100

    Fixes for Numba >= 0.49

--- adgm/utils.py
+++ adgm/utils.py
@@ -131,7 +131,7 @@ def simplex_projection_sparse(data, indices, indptr, num_vectors):
         ni = end - start
         a = -np.sort(-ci)
         lambdas = (np.cumsum(a) - 1)/np.arange(1, ni+1)  
-        xi = 1
+        xi = np.ones(1, )
         for k in range(ni-1, -1, -1):
             if a[k] > lambdas[k]:
                 xi = np.maximum(ci - lambdas[k], 0)
@@ -164,7 +164,7 @@ def simplex_projection_inequality_sparse(data, indices, indptr, num_vectors):
             ni = end - start
             a = -np.sort(-ci)
             lambdas = (np.cumsum(a) - 1)/np.arange(1, ni+1)
-            xi = 1
+            xi = np.ones(1, )
             for k in range(ni-1, -1, -1):
                 if a[k] > lambdas[k]:
                     xi = np.maximum(ci - lambdas[k], 0)
--- tests/test_numba.py
+++ tests/test_numba.py
@@ -1,7 +1,11 @@
+import sys, os
 import numpy as np
 import time
 import numba
 import scipy.sparse
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+from adgm import utils
 
 def simplex_projection(c):
     """
@@ -60,41 +64,11 @@ def benchmark(m=500, n=500, repeat=5):
         print('{}) numpy: {:6f}, numba: {:6f}'.format(i + 1, numpy_time, numba_time))
 
 
-@numba.jit(nopython=True, parallel=True, cache=True)
-def simplex_projection_inequality_sparse2(data, indices, indptr, num_vectors):
-    """
-    simplex project (with inequality constraints) of each row or each column of a sparse matrix C
-    If C is CSR: each row; if C is CSC: each column
-    data, indices, indptr, shape: representation of C (same notation as scipy csr/csc matrix)
-    num_vectors = number of rows if C is CSR
-                = number of cols if C is CSC
-    """
-    x = np.zeros(len(data))
-    for i in numba.prange(num_vectors):
-        # projection for each row independently
-        start = indptr[i]
-        end = indptr[i+1]
-        ci = data[start:end]
-        u = np.maximum(ci, 0)
-        if np.sum(u) <= 1:
-            xi = u
-        else:
-            ni = end - start
-            a = -np.sort(-ci)
-            lambdas = (np.cumsum(a) - 1)/np.arange(1, ni+1)  
-            for k in range(ni-1, -1, -1):
-                if a[k] > lambdas[k]:
-                    xi = np.maximum(ci - lambdas[k], 0)
-                    break
-        x[start:end] = xi
-    return x
-
-
 def test_simplex_projection():
     A = scipy.sparse.random(4, 5, density=0.5, format='csr')
-    x = simplex_projection_inequality_sparse2(A.data, A.indices, A.indptr, A.shape[0])
+    # x = simplex_projection_inequality_sparse2(A.data, A.indices, A.indptr, A.shape[0])
+    x = utils.simplex_projection_inequality_sparse(A.data, A.indices, A.indptr, A.shape[0])
     print(x)
-
 
 
 @numba.jit(nopython=True)
